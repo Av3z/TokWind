b05e66118be19f0ca1d131ba8f470fc16b75b722
Av3z <willyanf17@hotmail.com>
2023-11-30 13:52:01 -0300

    feat: add routes for control the navigation

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { StatusBar } from "react-native";
+import { Routes } from "./src/routes";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <NavigationContainer>
+    <StatusBar backgroundColor={'transparent'} barStyle="light-content" translucent={true} />
+    <Routes />
+   </NavigationContainer>
   );
 }
