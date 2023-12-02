f19989458e0dcdf037d13a49667f6ba73bdd2260
Av3z <willyanf17@hotmail.com>
2023-12-02 17:21:53 -0300

    feat: create all structure for home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,63 @@
-import { View, Text } from "react-native"
+import { View, Text, TouchableOpacity, FlatList } from "react-native"
+import { styled } from "nativewind"
+import { FeedItem } from "../../components/FeedItem";
+import { useState, useRef } from "react";
 
 export function Home(){
-    <View>
-        <Text>Pagina Home</Text>
-    </View>
+
+    const StyledView = styled(View);
+    const StyledText = styled(Text);
+    const StyledButton = styled(TouchableOpacity);
+
+    let feedItems = [ 
+        {
+          id: '1', 
+          video: 'https://i.imgur.com/Cnz1CPK.mp4',
+          name: '@sujeitoprogramador',
+          description: 'Criando o ShortDev do zero com RN',
+         },
+        {
+          id: '2', 
+          video: 'https://i.imgur.com/E0tK2bY.mp4',
+          name: '@henriquesilva',
+          description: 'Fala turma, estou aprendendo React Native com sujeito programador',
+         },
+        {
+          id: '3', 
+          video: 'https://i.imgur.com/mPFvFyX.mp4',
+          name: '@sujeitoprogramador',
+          description: 'Aprendendo a trabalhar com Drag and Drop no React Native',
+         }
+      ]
+
+      const [showItem, setShowItem] = useState(feedItems[0]);
+
+      const onViewRef = useRef(({ viewableItems }) => {
+
+        if(viewableItems && viewableItems.length > 0 ){
+            setShowItem(feedItems[viewableItems[0].index])
+            console.log(showItem)
+        }
+      });
+
+    return(
+        <StyledView className="bg-black">
+            <StyledView className="flex-row items-center justify-center right-0 left-0 top-10 gap-2 mt-5 z-50">
+                <StyledButton>
+                    <StyledText className="text-white font-bold mb-1">Seguindo</StyledText>
+                </StyledButton>
+
+                <StyledButton>
+                    <StyledText className="text-white font-bold mb-1">Pra você</StyledText>
+                    <StyledView className="self-center bg-white h-1 w-4"></StyledView>
+                </StyledButton>
+            </StyledView>
+
+            <FlatList 
+                data={feedItems}
+                renderItem={ ({item}) => <FeedItem data={item} currentVisible={showItem}/>}
+                onViewableItemsChanged={onViewRef.current}
+            />
+        </StyledView>
+    )
 }
